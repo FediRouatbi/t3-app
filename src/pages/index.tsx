4ba239dbb749dd68db295ac13dc5741e183b3427
fedi <fedirouatbi@gmail.com>
2023-03-28 00:07:55 +0100

    add post to ctx

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -7,6 +7,8 @@ import dayjs from "dayjs";
 import relativeTime from "dayjs/plugin/relativeTime";
 import CustomImage from "~/components/CustomImage";
 import LoadingPage from "~/components/Spinner";
+import type { FormEvent } from "react";
+import { useState } from "react";
 
 dayjs.extend(relativeTime);
 type PostWithUser = RouterOutputs["posts"]["getAll"][number];
@@ -30,6 +32,7 @@ const PostView = (post: PostWithUser) => {
 
 const Posts = () => {
   const { data, isLoading } = api.posts.getAll.useQuery();
+  console.log(data, isLoading);
 
   if (isLoading) return <LoadingPage />;
 
@@ -45,10 +48,21 @@ const Posts = () => {
 };
 
 const Home: NextPage = () => {
+  const [input, setInput] = useState("");
   const { user, isSignedIn } = useUser();
-
   api.posts.getAll.useQuery();
 
+  const { mutate, isLoading: isPosting } = api.posts.post.useMutation({
+    onSuccess: () => {
+      setInput("");
+      void api.useContext().posts.invalidate();
+    },
+  });
+
+  const handelPost = (e: FormEvent<HTMLFormElement>) => {
+    e.preventDefault();
+    mutate({ content: input });
+  };
   return (
     <>
       <Head>
@@ -63,15 +77,21 @@ const Home: NextPage = () => {
             <div className="absolute top-10 right-10">
               <UserButton />
             </div>
-            <div className="flex items-center space-x-4">
+            <form className="flex items-center space-x-4" onSubmit={handelPost}>
               <CustomImage image={user?.profileImageUrl} />
               <input
                 type="text"
                 className="block w-full rounded-lg bg-gray-50  p-2.5 text-sm text-gray-900 outline-none focus:border-blue-500 focus:ring-blue-500 dark:border-gray-600 dark:bg-gray-700 dark:text-white dark:placeholder-gray-400 dark:focus:border-blue-500 dark:focus:ring-blue-500"
                 placeholder="Start a post"
                 required
+                value={input}
+                onChange={(e) => setInput(e.target.value)}
+                disabled={isPosting}
               />
-            </div>
+              <button className="rounded-lg bg-gray-700 px-3 py-2 text-center text-xs font-medium text-white hover:bg-gray-800 focus:outline-none focus:ring-4 focus:ring-gray-300 dark:bg-gray-600 dark:hover:bg-gray-700 dark:focus:ring-gray-800">
+                Post
+              </button>
+            </form>
             <Posts />
           </>
         ) : (
--- src/server/api/routers/post.ts
+++ src/server/api/routers/post.ts
@@ -1,3 +1,4 @@
+import { privateProcedure } from "./../trpc";
 import { clerkClient } from "@clerk/nextjs/server";
 import { z } from "zod";
 
@@ -5,7 +6,10 @@ import { createTRPCRouter, publicProcedure } from "~/server/api/trpc";
 
 export const postsRouter = createTRPCRouter({
   getAll: publicProcedure.query(async ({ ctx }) => {
-    const posts = await ctx.prisma.post.findMany({ take: 100 });
+    const posts = await ctx.prisma.post.findMany({
+      take: 100,
+      orderBy: { createdAt: "desc" },
+    });
     const users = (
       await clerkClient.users.getUserList({
         userId: posts.map((post) => post.authorId),
@@ -19,11 +23,24 @@ export const postsRouter = createTRPCRouter({
       lastName: user.lastName,
     }));
 
-    return posts
-      .map((post) => ({
-        post,
-        author: users.find((user) => user.id === post.authorId),
-      }))
-      .reverse();
+    return posts.map((post) => ({
+      post,
+      author: users.find((user) => user.id === post.authorId),
+    }));
   }),
+  post: privateProcedure
+    .input(
+      z.object({
+        content: z.string().min(4),
+      })
+    )
+    .mutation(async ({ ctx, input }) => {
+      const authorId = ctx.authorId;
+
+      const post = await ctx.prisma.post.create({
+        data: { content: input.content, authorId },
+      });
+
+      return post;
+    }),
 });
